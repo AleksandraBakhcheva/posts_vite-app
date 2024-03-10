6471a30f7fd9bf74a4841b9dd9263f3dc37db704
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-03-10 21:28:57 +0300

    add NotFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,28 @@
+import styles from "./NotFoundPage.module.css";
+import error404 from "../../assets/images/error404.svg";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+export const NotFoundPage = () => {
+  return (
+    <main className={styles.container}>
+      <div>
+        <h2>Ooops!</h2>
+        <h3>Error 404 - PAGE NOT FOUND</h3>
+        <p>The page you are looking for might have been removed</p>
+        <p>had its name changed or is temporarily unavailable.</p>
+        {<img src={error404} alt="error-404" />}
+        <div>
+          <Link to="/">
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
+            >
+              Homepage
+            </motion.button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
