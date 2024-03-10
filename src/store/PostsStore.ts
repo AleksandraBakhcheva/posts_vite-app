5deced71cde79b19dde4a0853478a5639b17ae13
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-03-11 00:51:18 +0300

    update store + vars

--- src/store/PostsStore.ts
+++ src/store/PostsStore.ts
@@ -10,9 +10,7 @@ class PostsStore {
 
   getPosts = async () => {
     this.isLoading = true;
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?_page=0&_limit=10"
-    );
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     this.posts = await response.json();
     this.isLoading = false;
   };
