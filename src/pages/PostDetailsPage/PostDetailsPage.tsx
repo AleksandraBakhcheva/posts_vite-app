b8ab915b97d71f01d3581f4d2a3ab4abc976c527
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-03-10 21:29:59 +0300

    add PostDetailsPage

---
+++ src/pages/PostDetailsPage/PostDetailsPage.tsx
@@ -0,0 +1,47 @@
+import styles from "./PostDetailsPage.module.css";
+import { useParams } from "react-router-dom";
+import store from "../../store/PostsStore.js";
+import { IPost } from "../../utils/Interfaces.js";
+import { Header } from "../../components/Header/Header.js";
+import { Footer } from "../../components/Footer/Footer.js";
+
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+export const PostDetailsPage = () => {
+  const { id } = useParams();
+
+  let post: IPost = {
+    userId: 0,
+    id: 0,
+    title: "",
+    body: "",
+  };
+
+  if (store.posts) {
+    post = store.posts.find((item: { id: string }) => item.id == id)!;
+  }
+
+  return (
+    <>
+      <Header />
+      <main className={styles.post}>
+        <div className={styles.post_box}>
+          <h3>Post Id: {post.id}</h3>
+          <h2 className={styles.header}>Post Title: "{post.title}"</h2>
+          <p>Extract: "{post.body}..."</p>
+          <Link to="/">
+            <motion.button
+              className="details-button"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9, x: "-5px", y: "5px" }}
+            >
+              Homepage
+            </motion.button>
+          </Link>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
